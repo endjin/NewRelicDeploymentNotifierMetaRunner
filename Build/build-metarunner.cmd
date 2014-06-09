01112a958a91140aa45f74d5d7ade7995c5459fa
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2014-06-09 21:31:16 +0100

    Initial commit of NewRelic Deployment Notifier MetaRunner for TeamCity

---
+++ Build/build-metarunner.cmd
@@ -0,0 +1,15 @@
+MKDIR %~dp0..\Drops
+MKDIR %~dp0..\Drops\Temp
+
+XCOPY /E /Y "%~dp0..\Solutions\*.*" "%~dp0..\Drops\Temp\"
+
+7z a "%~dp0..\Drops\Temp\agent\NewRelicDeploymentNotifier_plugin.zip" "%~dp0..\Drops\Temp\agent\**\*.*"
+
+DEL "%~dp0..\Drops\Temp\agent\*.xml"
+RMDIR "%~dp0..\Drops\Temp\agent\bin" /S /Q
+
+7z a "%~dp0..\Drops\NewRelicDeploymentNotifier.zip" "%~dp0..\Drops\Temp\**"
+
+RMDIR "%~dp0..\Drops\Temp\" /S /Q
+
+PAUSE
