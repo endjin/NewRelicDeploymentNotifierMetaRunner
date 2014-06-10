22246c213bdbf29e416ac7fcdeedbaee55e1748d
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2014-06-10 12:32:18 +0100

    Update the output package name

--- Build/build-metarunner.cmd
+++ Build/build-metarunner.cmd
@@ -3,7 +3,7 @@ MKDIR %~dp0..\Drops\Temp
 
 XCOPY /E /Y "%~dp0..\Solutions\*.*" "%~dp0..\Drops\Temp\"
 
-7z a "%~dp0..\Drops\Temp\agent\NewRelicDeploymentNotifier_plugin.zip" "%~dp0..\Drops\Temp\agent\**\*.*"
+7z a "%~dp0..\Drops\Temp\agent\nr_deployment_notifier.zip" "%~dp0..\Drops\Temp\agent\**\*.*"
 
 DEL "%~dp0..\Drops\Temp\agent\*.xml"
 RMDIR "%~dp0..\Drops\Temp\agent\bin" /S /Q
