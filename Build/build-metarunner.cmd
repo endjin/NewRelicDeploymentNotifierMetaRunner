c6610544a710fb6a1e8411d3cb87b98c9716e57c
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2014-06-12 14:42:03 +0100

    Create two seperate examples - a simple runner and powershell runner

--- Build/build-metarunner.cmd
+++ Build/build-metarunner.cmd
@@ -3,13 +3,11 @@ MKDIR %~dp0..\Drops\Temp
 
 XCOPY /E /Y "%~dp0..\Solutions\*.*" "%~dp0..\Drops\Temp\"
 
-7z a "%~dp0..\Drops\Temp\agent\nr_deployment_notifier.zip" "%~dp0..\Drops\Temp\agent\**\*.*"
+7z a -tzip -mx0 "%~dp0..\Drops\Temp\agent\nr_deployment_notifier.zip" "%~dp0..\Drops\Temp\agent\**\*.*"
 
 DEL "%~dp0..\Drops\Temp\agent\*.xml"
 RMDIR "%~dp0..\Drops\Temp\agent\bin" /S /Q
 
-7z a "%~dp0..\Drops\NewRelicDeploymentNotifier.zip" "%~dp0..\Drops\Temp\**"
+7z a -tzip -mx0 "%~dp0..\Drops\newrelic-deployment-notifier.zip" "%~dp0..\Drops\Temp\**"
 
 RMDIR "%~dp0..\Drops\Temp\" /S /Q
-
-PAUSE
