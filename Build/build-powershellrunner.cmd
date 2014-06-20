0d01fcd43e32a255d6841f6723f6bf57baf74eb5
Howard van Rooijen <Howard.vanRooijen@endjin.com>
2014-06-20 14:21:28 +0100

    Update Build Scripts to work with a PowerShell based zip creation script.

--- Build/build-powershellrunner.cmd
+++ Build/build-powershellrunner.cmd
@@ -3,11 +3,13 @@ MKDIR %~dp0..\Drops\Temp\PowerShellRunner
 
 XCOPY /E /Y "%~dp0..\Solutions\PowerShellRunner\*.*" "%~dp0..\Drops\Temp\PowerShellRunner"
 
-7z a -tzip -mx0 "%~dp0..\Drops\Temp\PowerShellRunner\agent\nr_deployment_notifier.zip" "%~dp0..\Drops\Temp\PowerShellRunner\agent\**\*.*"
+@PowerShell -File "%~dp0Create-Zip.ps1" %~dp0..\Drops\Temp\PowerShellRunner\agent\ %~dp0..\Drops\Temp\PowerShellRunner\agent\nr_deployment_notifier.zip
 
 DEL "%~dp0..\Drops\Temp\PowerShellRunner\agent\*.xml"
 RMDIR "%~dp0..\Drops\Temp\PowerShellRunner\agent\bin" /S /Q
 
-7z a -tzip -mx0 "%~dp0..\Drops\newrelic-deployment-notifier.zip" "%~dp0..\Drops\Temp\**"
+@PowerShell -File "%~dp0Create-Zip.ps1" -target %~dp0..\Drops\Temp\PowerShellRunner\ %~dp0..\Drops\newrelic-deployment-notifier.zip
 
 RMDIR "%~dp0..\Drops\Temp\" /S /Q
+
+PAUSE
--- Build/build-simplerunner.cmd
+++ Build/build-simplerunner.cmd
@@ -3,11 +3,11 @@ MKDIR %~dp0..\Drops\Temp\SimpleRunner
 
 XCOPY /E /Y "%~dp0..\Solutions\SimpleRunner\*.*" "%~dp0..\Drops\Temp\SimpleRunner"
 
-7z a -tzip -mx0 "%~dp0..\Drops\Temp\SimpleRunner\agent\nr_deployment_notifier.zip" "%~dp0..\Drops\Temp\SimpleRunner\agent\**\*.*"
+@PowerShell -File "%~dp0Create-Zip.ps1" %~dp0..\Drops\Temp\SimpleRunner\agent\ %~dp0..\Drops\Temp\SimpleRunner\agent\nr_deployment_notifier.zip
 
 DEL "%~dp0..\Drops\Temp\SimpleRunner\agent\*.xml"
 RMDIR "%~dp0..\Drops\Temp\SimpleRunner\agent\bin" /S /Q
 
-7z a -tzip -mx0 "%~dp0..\Drops\newrelic-deployment-notifier.zip" "%~dp0..\Drops\Temp\**"
+@PowerShell -File "%~dp0Create-Zip.ps1" -target %~dp0..\Drops\Temp\SimpleRunner\ %~dp0..\Drops\newrelic-deployment-notifier.zip
 
 RMDIR "%~dp0..\Drops\Temp\" /S /Q
